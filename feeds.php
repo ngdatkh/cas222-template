2721768ebfb7c1c52eb68dd962915806208b44d5
Dat Nguyen <ngdatkh@gmail.com>
2018-02-05 17:04:56 -0800

    Initial commit

---
+++ feeds.php
@@ -0,0 +1,13 @@
+<?php
+  echo '<section>
+      <section>
+        <span>Weather feed here</span>
+      </section>
+      <section>
+        <span>Media feed here</span>
+      </section>
+      <section>
+        <span>Media feed here</span>
+      </section>
+    </section>    ';
+?>
